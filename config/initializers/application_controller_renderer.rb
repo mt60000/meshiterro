0100a8fb4a1bf9400682d1e502e6955ff9ac384e
mt60000 <niruyaku45@gmail.com>
2021-08-04 11:27:03 +0000

    4章完了

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '83c68e4e10f36a58a79675ee721d900ca5b79b733308f7c776671fb37115887b6da157cd1cee0e76c9c177ca0356d6b9ad1c98c3e5345dde236dcb2e791d3f3e'
